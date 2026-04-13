9426613a436b3299d14ccc5578c2fa95ec15d4a4
mks8600 <mkssingh8600@gmail.com>
2026-04-13 22:28:33 +0530

    Add console log versioning to easily verify deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+console.log('=============================================');
+console.log('DEPLOYMENT VERSION: POSTGRES_API_V3 (ACTIVE)');
+console.log('=============================================');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
